c77e3c5272188c7baeda30f1cb8244e7c4c7fe88
jimmy <jimewilson87@gmail.com>
2020-10-25 21:44:38 -0500

    adds HTML theme

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,449 @@
 <template>
   <div id="app">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/">Movies</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/movies">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/movies">All Movies</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/movies/new">New Movie</router-link>
-          </li>
-          <li class="nav-item" v-if="!isLoggedIn()">
-            <router-link class="nav-link" to="/signup">Signup</router-link>
-          </li>
-          <li class="nav-item" v-if="!isLoggedIn()">
-            <router-link class="nav-link" to="/login">Login</router-link>
-          </li>
-          <li class="nav-item" v-if="isLoggedIn()">
-            <router-link class="nav-link" to="/logout">Logout</router-link>
-          </li>
-        </ul>
+    <!-- Header -->
+    <div id="header">
+      <!-- Inner -->
+      <div class="inner">
+        <header>
+          <h1><a href="index.html" id="logo">My Movies</a></h1>
+          <hr />
+          <p>The greatest movies in the world (and possibly the universe)</p>
+        </header>
+        <footer>
+          <a href="#banner" class="button circled scrolly">Start</a>
+        </footer>
       </div>
-    </nav>
 
-    <div class="container">
-      <router-view/>
+      <!-- Nav -->
+      <nav id="nav">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li>
+            <a href="no-sidebar.html">Add Movie</a>
+            <!-- <ul>
+              <li><a href="#">Lorem ipsum dolor</a></li>
+              <li><a href="#">Magna phasellus</a></li>
+              <li><a href="#">Etiam dolore nisl</a></li>
+              <li>
+                <a href="#">And a submenu &hellip;</a>
+                <ul>
+                  <li><a href="#">Lorem ipsum dolor</a></li>
+                  <li><a href="#">Phasellus consequat</a></li>
+                  <li><a href="#">Magna phasellus</a></li>
+                  <li><a href="#">Etiam dolore nisl</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Veroeros feugiat</a></li>
+            </ul> -->
+          </li>
+          <li><a href="left-sidebar.html">Edit Movie</a></li>
+          <li><a href="right-sidebar.html">Account</a></li>
+          <li><a href="#">Nowhere</a></li>
+        </ul>
+      </nav>
     </div>
-    
+
+    <!-- Banner -->
+    <section id="banner">
+      <header>
+        <h2>Oh hello. Here are the <strong>Movies</strong>.</h2>
+        <p>
+          Feel free to take a peek.
+        </p>
+      </header>
+    </section>
+
+    <!-- Carousel -->
+    <section class="carousel">
+      <div class="reel">
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic01.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Jurassic Park</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic02.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Good Burger</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic03.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Kazaam</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic04.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Mac and Me</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic05.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">3 Ninjas</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic01.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Night at the Roxbury</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic02.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Parasite</a></h3>
+          </header>
+          <!-- <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p> -->
+        </article>
+
+        <!-- <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic03.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+          </header>
+          <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic04.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Ultrices urna sit lobortis</a></h3>
+          </header>
+          <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article>
+
+        <article>
+          <a href="#" class="image featured"
+            ><img src="images/pic05.jpg" alt=""
+          /></a>
+          <header>
+            <h3><a href="#">Varius magnis sollicitudin</a></h3>
+          </header>
+          <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+        </article> -->
+      </div>
+    </section>
+
+    <!-- Main -->
+    <div class="wrapper style2">
+      <article id="main" class="container special">
+        <a href="#" class="image featured"
+          ><img src="images/pic06.jpg" alt=""
+        /></a>
+        <header>
+          <h2><a href="#">About the movie selection</a></h2>
+          <p>
+            Okay. The first one and last one are actually good films. <br />
+            But the rest, they are without a doubt the greatest pieces of cinema
+            ever created.
+          </p>
+        </header>
+        <!-- <p>
+          Commodo id natoque malesuada sollicitudin elit suscipit. Curae
+          suspendisse mauris posuere accumsan massa posuere lacus convallis
+          tellus interdum. Amet nullam fringilla nibh nulla convallis ut
+          venenatis purus sit arcu sociis. Nunc fermentum adipiscing tempor
+          cursus nascetur adipiscing adipiscing. Primis aliquam mus lacinia
+          lobortis phasellus suscipit. Fermentum lobortis non tristique ante
+          proin sociis accumsan lobortis. Auctor etiam porttitor phasellus
+          tempus cubilia ultrices tempor sagittis. Nisl fermentum consequat
+          integer interdum integer purus sapien. Nibh eleifend nulla nascetur
+          pharetra commodo mi augue interdum tellus. Ornare cursus augue feugiat
+          sodales velit lorem. Semper elementum ullamcorper lacinia natoque
+          aenean scelerisque.
+        </p> -->
+        <footer>
+          <!-- <a href="#" class="button">Continue Reading</a> -->
+        </footer>
+      </article>
+    </div>
+
+    <!-- Features -->
+    <!-- <div class="wrapper style1">
+      <section id="features" class="container special">
+        <header>
+          <h2>Morbi ullamcorper et varius leo lacus</h2>
+          <p>
+            Ipsum volutpat consectetur orci metus consequat imperdiet duis
+            integer semper magna.
+          </p>
+        </header>
+        <div class="row">
+          <article class="col-4 col-12-mobile special">
+            <a href="#" class="image featured"
+              ><img src="images/pic07.jpg" alt=""
+            /></a>
+            <header>
+              <h3><a href="#">Gravida aliquam penatibus</a></h3>
+            </header>
+            <p>
+              Amet nullam fringilla nibh nulla convallis tique ante proin sociis
+              accumsan lobortis. Auctor etiam porttitor phasellus tempus cubilia
+              ultrices tempor sagittis. Nisl fermentum consequat integer
+              interdum.
+            </p>
+          </article>
+          <article class="col-4 col-12-mobile special">
+            <a href="#" class="image featured"
+              ><img src="images/pic08.jpg" alt=""
+            /></a>
+            <header>
+              <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+            </header>
+            <p>
+              Amet nullam fringilla nibh nulla convallis tique ante proin sociis
+              accumsan lobortis. Auctor etiam porttitor phasellus tempus cubilia
+              ultrices tempor sagittis. Nisl fermentum consequat integer
+              interdum.
+            </p>
+          </article>
+          <article class="col-4 col-12-mobile special">
+            <a href="#" class="image featured"
+              ><img src="images/pic09.jpg" alt=""
+            /></a>
+            <header>
+              <h3><a href="#">Magna laoreet et aliquam</a></h3>
+            </header>
+            <p>
+              Amet nullam fringilla nibh nulla convallis tique ante proin sociis
+              accumsan lobortis. Auctor etiam porttitor phasellus tempus cubilia
+              ultrices tempor sagittis. Nisl fermentum consequat integer
+              interdum.
+            </p>
+          </article>
+        </div>
+      </section>
+    </div> -->
+    <div class="container">
+      <router-view />
+      <!-- Footer -->
+      <div id="footer">
+        <div class="container">
+          <div class="row">
+            <!-- Tweets -->
+            <section class="col-4 col-12-mobile">
+              <header>
+                <h2 class="icon brands fa-twitter circled">
+                  <span class="label">Tweets</span>
+                </h2>
+              </header>
+              <ul class="divided">
+                <li>
+                  <article class="tweet">
+                    Agreed. Best movies of all time.
+                    <span class="timestamp">5 minutes ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article class="tweet">
+                    GOAT.
+                    <span class="timestamp">30 minutes ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article class="tweet">
+                    My life is now changed for the better. Thank you.
+                    <span class="timestamp">3 hours ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article class="tweet">
+                    Movies.
+                    <span class="timestamp">5 hours ago</span>
+                  </article>
+                </li>
+              </ul>
+            </section>
+
+            <!-- Posts -->
+            <section class="col-4 col-12-mobile">
+              <header>
+                <h2 class="icon solid fa-file circled">
+                  <span class="label">Posts</span>
+                </h2>
+              </header>
+              <ul class="divided">
+                <li>
+                  <article class="post stub">
+                    <header>
+                      <h3><a href="#">Worst movies of all time</a></h3>
+                    </header>
+                    <span class="timestamp">3 hours ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article class="post stub">
+                    <header>
+                      <h3><a href="#">Every movie ever</a></h3>
+                    </header>
+                    <span class="timestamp">6 hours ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article class="post stub">
+                    <header>
+                      <h3><a href="#">Best TV</a></h3>
+                    </header>
+                    <span class="timestamp">Yesterday</span>
+                  </article>
+                </li>
+                <li>
+                  <article class="post stub">
+                    <header>
+                      <h3><a href="#">Worst TV</a></h3>
+                    </header>
+                    <span class="timestamp">2 days ago</span>
+                  </article>
+                </li>
+              </ul>
+            </section>
+
+            <!-- Photos -->
+            <section class="col-4 col-12-mobile">
+              <header>
+                <h2 class="icon solid fa-camera circled">
+                  <span class="label">Photos</span>
+                </h2>
+              </header>
+              <div class="row gtr-25">
+                <div class="col-6">
+                  <a href="#" class="image fit"
+                    ><img src="images/pic10.jpg" alt=""
+                  /></a>
+                </div>
+                <div class="col-6">
+                  <a href="#" class="image fit"
+                    ><img src="images/pic11.jpg" alt=""
+                  /></a>
+                </div>
+                <div class="col-6">
+                  <a href="#" class="image fit"
+                    ><img src="images/pic12.jpg" alt=""
+                  /></a>
+                </div>
+                <div class="col-6">
+                  <a href="#" class="image fit"
+                    ><img src="images/pic13.jpg" alt=""
+                  /></a>
+                </div>
+                <div class="col-6">
+                  <a href="#" class="image fit"
+                    ><img src="images/pic14.jpg" alt=""
+                  /></a>
+                </div>
+                <div class="col-6">
+                  <a href="#" class="image fit"
+                    ><img src="images/pic15.jpg" alt=""
+                  /></a>
+                </div>
+              </div>
+            </section>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col-12">
+              <!-- Contact -->
+              <section class="contact">
+                <header>
+                  <h3>Wanna fight about it?</h3>
+                </header>
+                <p>
+                  There's no discussion.
+                </p>
+                <ul class="icons">
+                  <li>
+                    <a href="#" class="icon brands fa-twitter"
+                      ><span class="label">Twitter</span></a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="icon brands fa-facebook-f"
+                      ><span class="label">Facebook</span></a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="icon brands fa-instagram"
+                      ><span class="label">Instagram</span></a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="icon brands fa-pinterest"
+                      ><span class="label">Pinterest</span></a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="icon brands fa-dribbble"
+                      ><span class="label">Dribbble</span></a
+                    >
+                  </li>
+                  <li>
+                    <a href="#" class="icon brands fa-linkedin-in"
+                      ><span class="label">Linkedin</span></a
+                    >
+                  </li>
+                </ul>
+              </section>
+
+              <!-- Copyright -->
+              <div class="copyright">
+                <ul class="menu">
+                  <li>&copy; Untitled. All rights reserved.</li>
+                  <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
   data: function() {
-    return {
-    };
+    return {};
   },
   methods: {
     isLoggedIn: function() {
       return localStorage.getItem("jwt");
-    }
-  }
+    },
+  },
 };
 </script>
